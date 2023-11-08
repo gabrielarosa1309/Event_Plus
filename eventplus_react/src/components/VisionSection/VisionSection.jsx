cafa3f108a007df0cdb9f558391dfbf2a4648891
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-11-08 10:54:18 -0300

    :construction: Home quase finalizada

--- eventplus_react/src/components/VisionSection/VisionSection.jsx
+++ eventplus_react/src/components/VisionSection/VisionSection.jsx
@@ -6,7 +6,7 @@ const VisionSection = () => {
     return (
         <section className='vision'>
             <div className='vision__box'>
-                <Title titleText={"Visão"} color='white' marginTop='vision_tite' />
+                <Title titleText={"Visão"} color='white' />
                 <p className='vision__text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem neque, aperiam nihil maiores perspiciatis illum facere atque dolores asperiores ab iure nemo blanditiis quo. Culpa ut ducimus vel molestiae temporibus!</p>
             </div>
         </section>
--- eventplus_react/src/pages/HomePage/HomePage.jsx
+++ eventplus_react/src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,54 @@
 import React from 'react';
 import './HomePage.css';
-// import Title from '../../components/Title/Title';
-import Banner from '../../components/Banner/Banner';
-import VisionSection from '../../components/VisionSection/VisionSection';
+import Title from '../../components/Title/Title';
 import MainContent from '../../components/Main/MainContent';
+import Banner from '../../components/Banner/Banner';
+import NextEvent from '../../components/NextEvent/NextEvent';
+import Container from '../../components/Container/Container';
+import VisionSection from '../../components/VisionSection/VisionSection';
+import ContactSection from '../../components/ContactSection/ContactSection';
 
 const HomePage = () => {
     return (
         <div>
-            {/* <Title titleText="Página Home" marginTop='margin_top'/> */}
             <MainContent>
-                <Banner />
+                <Banner/>
+
+                <section className='proximos-eventos'>
+                    <Container>
+                        <Title titleText={"Próximos Eventos"}/>
+
+                        <div className='events-box'>
+                            <NextEvent
+                                title={"Evento X"}
+                                description={"Evento legal"}
+                                eventDate={"10/11/2023"}
+                                idEvent={"JKSNDXKJD"}
+                            />
+                            <NextEvent
+                                title={"Evento Y"}
+                                description={"Evento nem tão legal"}
+                                eventDate={"11/11/2023"}
+                                idEvent={"KJFKGRG"}
+                            />
+                            <NextEvent
+                                title={"Evento Z"}
+                                description={"Evento mega legal"}
+                                eventDate={"12/11/2023"}
+                                idEvent={"SDLFMLFLK"}
+                            />
+                            <NextEvent
+                                title={"Evento W"}
+                                description={"Evento um pouco legal"}
+                                eventDate={"13/11/2023"}
+                                idEvent={"LKJIJFIHF"}
+                            />
+                        </div>
+                    </Container>
+                </section>
+
                 <VisionSection/>
+                <ContactSection/>
             </MainContent>
         </div>
     );
